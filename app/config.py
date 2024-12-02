9fe97ff5668ff73405f1973de94e4619ed79bacc
Josz009 <108372810+Josz009@users.noreply.github.com>
2024-12-02 17:13:22 -0600

    Update config.py
    config.py variables

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 
 class Config:
-    SECRET_KEY = "your-secret-key"
+    SECRET_KEY = "2b334e4a92f8f41f79dda2eea485d60370f20e9df822fd99462bab9b3994cb27"
     DATABASE_URI = "sqlite:///database/scans.db"
     METASPLOIT_RPC_HOST = "127.0.0.1"
     METASPLOIT_RPC_PORT = 55553
     METASPLOIT_USER = "msf"
-    METASPLOIT_PASS = "yourpassword"
+    METASPLOIT_PASS = "msf"
